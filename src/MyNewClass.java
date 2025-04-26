67bc2c999e7cd0637f2ccde743d17ab93b7a0d62
Anas-Jarrar <anasjarrar944@gmail.com>
2025-04-26 03:42:02 +0300

    Last Updates up to CheckBoxTest

--- src/MyNewClass.java
+++ src/MyNewClass.java
@@ -1,19 +1,27 @@
 import static org.testng.Assert.assertEquals;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.awt.RenderingHints.Key;
 import java.util.List;
+import java.util.Random;
 
 public class MyNewClass {
 
 	String TheWebsite = "https://codenboxautomationlab.com/practice/";
 	WebDriver driver = new ChromeDriver();
+	Random rand = new Random();
 
 	@BeforeTest
 	public void Setup() {
@@ -22,7 +30,7 @@ public class MyNewClass {
 
 	}
 
-	@Test(priority = 1)
+	@Test(priority = 1, enabled = false)
 	public void RadioButton() throws InterruptedException {
 		WebElement Radio1 = driver.findElement(By.cssSelector(".radioButton"));
 		WebElement Radio2 = driver.findElement(By.xpath("//input[@value='radio2']"));
@@ -41,7 +49,7 @@ public class MyNewClass {
 
 	}
 
-	@Test(priority = 2)
+	@Test(priority = 2, enabled = false)
 	// This Test search between array of countries and seen if exist
 	public void DynamicDropDown() throws InterruptedException {
 
@@ -67,7 +75,7 @@ public class MyNewClass {
 		}
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 3, enabled = false)
 	// Print The Dynamic Drop down list based on array of suggested letters
 	public void PrintDynamicDropDown() throws InterruptedException {
 
@@ -87,41 +95,85 @@ public class MyNewClass {
 	}
 
 	@Test(priority = 4)
+	public void AutoCompleteTest() throws InterruptedException {
+
+		String[] countries = { "jo", "ja", "sy" };
+		WebElement AutoComplete = driver.findElement(By.id("autocomplete"));
+		int index = rand.nextInt(countries.length);
+		System.out.println(index);
+		AutoComplete.sendKeys(countries[index]);
+		Thread.sleep(1000);
+		AutoComplete.sendKeys(Keys.chord(Keys.ARROW_DOWN, Keys.ENTER));
+
+	}
+	
+	
+
+	@Test(priority = 5,enabled=false)
+	public void StaticDropDown() throws InterruptedException {
+
+		WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
+		List<WebElement> options = dropdown.findElements(By.tagName("option"));
+
+		for (WebElement option : options) {
+			option.click();
+			System.out.println(option.getText());
+
+		}
+	}
+	@Test(priority = 6)
+	public void StaticList() {
+		WebElement dropdown = driver.findElement(By.id("dropdown-class-example"));
+		Select Myselect= new Select(dropdown);
+//		Myselect.selectByVisibleText("Appium");
+//		Myselect.selectByIndex(1);
+		Myselect.selectByValue("option3");
+		System.out.println(dropdown.findElements(By.tagName("option")).get(1).getText());
+		
+	}
+	
+	
+	@Test(priority=7,enabled=false)
+	public void CheckboxTest1() {
+		WebElement Checkbox =driver.findElement(By.id("checkbox-example"));
+		List<WebElement> options=Checkbox.findElements(By.tagName("input"));
+		int index=rand.nextInt(options.size());
+		options.get(index).click();
+	}
+	
+	@Test(priority = 8)
 	// This test to make sure every checkbox Buttons are worked
 	public void CheckboxTest() {
-
+		JavascriptExecutor js=(JavascriptExecutor)driver;
+		js.executeScript("window.scrollTo(0,200)");
+		WebElement Checkbox =driver.findElement(By.id("checkbox-example"));
+		List<WebElement> options=Checkbox.findElements(By.tagName("input"));
+		for(int i=0;i<options.size();i++) {
+			options.get(i).click();
+		}
 		WebElement Checkboxbutton1 = driver.findElement(By.xpath("//input[@name='checkBoxOption1']"));
 		WebElement Checkboxbutton2 = driver.findElement(By.xpath("//input[@name='checkBoxOption2']"));
 		WebElement Checkboxbutton3 = driver.findElement(By.xpath("//input[@name='checkBoxOption3']"));
-		Checkboxbutton1.click();
-		Checkboxbutton2.click();
-		Checkboxbutton3.click();
 
 		Assert.assertTrue(Checkboxbutton1.isSelected(), "The Checkboxbutton1 does not select");
 		Assert.assertTrue(Checkboxbutton2.isSelected(), "The Checkboxbutton2 does not select");
 		Assert.assertTrue(Checkboxbutton3.isSelected(), "The Checkboxbutton3 does not select");
 	}
-	
-	
-	
-	@Test(priority = 5)
-	//This Test for Select CheckBox Button based on his name
+
+	@Test(priority = 9, enabled = false)
+	// This Test for Select CheckBox Button based on his name
 	public void SelectSecondButtonOfCheckbox() {
-	    List<WebElement> options = driver.findElements(By.xpath("//input[@type='checkbox']"));
+		List<WebElement> options = driver.findElements(By.xpath("//input[@type='checkbox']"));
 
-	    for (WebElement option : options) {
-	        String value = option.getAttribute("value");  // values are like: option1, option2, etc.
-	        if (value.equalsIgnoreCase("option2")) {
-	            option.click();
-	            Assert.assertFalse(option.isSelected(), "Option2 checkbox should not be selected");
-	            break;
-	        }
-	    }
+		for (WebElement option : options) {
+			String value = option.getAttribute("value");
+			if (value.equalsIgnoreCase("option2")) {
+				option.click();
+				Assert.assertFalse(option.isSelected(), "Option2 checkbox should not be selected");
+				break;
+			}
+		}
 	}
-
-	
-	
-	
 	
 
 }
